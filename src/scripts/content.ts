b6be03bb378ba0e86495b0f5d3d22471d86fb095
bacowan <cowan.brendan.a@gmail.com>
2025-07-06 19:42:21 +0900

    tmp

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,8 +1,18 @@
 import { Readability } from "@mozilla/readability";
+//import kuromoji from "kuromoji";
 
-const parse = () => {
+const parse = async () => {
+  // only parse the page if it hasn't been visited before
+  const visitedKey = "visited_" + window.location.href;
+  const visited = chrome.storage.local.get(visitedKey);
+  
+  if (!visited) {
+    chrome.storage.local.set({ [visitedKey]: true });
     const article = new Readability(document.cloneNode(true) as Document).parse();
-    console.log(article);
+    if (article) {
+      
+    }
+  }
 }
 
 // Listen for changes in the URL in ways other than regular navigation.
@@ -12,3 +22,13 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
     parse();
   }
 });
+
+
+/*kuromoji.builder({ dicPath: "node_modules/kuromoji/dict" }).build((err, tokenizer) => {
+  if (err) throw err;
+
+  const tokens = tokenizer.tokenize("ご飯を食べました");
+  tokens.forEach(token => {
+    console.log(token.surface_form, "→", token.basic_form); // 食べました → 食べる
+  });
+});*/
