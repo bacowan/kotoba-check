bbc6b5f46b6411068c6d895eecdd27f1142a1a8f
bacowan <cowan.brendan.a@gmail.com>
2025-07-08 21:40:31 +0900

    removing dead code

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,17 +1,29 @@
 import { Readability } from "@mozilla/readability";
 import kuromoji from "kuromoji";
 
-const parse = async () => {
+const parse = () => {
+  const article = new Readability(document.cloneNode(true) as Document).parse();
+  if (article) {
+    kuromoji.builder({ dicPath: chrome.runtime.getURL('dict') }).build(function (err, tokenizer) {
+      // tokenizer is ready
+      const title = tokenizer.tokenize(article.title);
+      const content = tokenizer.tokenize(article.textContent);
+      console.log("start");
+      console.log(title);
+      console.log(content);
+    });
+  }
+}
+
+const checkPage = async () => {
   // only parse the page if it hasn't been visited before
   const visitedKey = "visited_" + window.location.href;
-  const visited = chrome.storage.local.get(visitedKey);
-  
-  if (!visited) {
-    chrome.storage.local.set({ [visitedKey]: true });
-    const article = new Readability(document.cloneNode(true) as Document).parse();
-    if (article) {
-      
-    }
+  const wasVisited = await chrome.storage.local.get([visitedKey]);
+  console.log(wasVisited[visitedKey]);
+
+  if (!wasVisited[visitedKey]) {
+    await chrome.storage.local.set({ [visitedKey]: true });
+    parse();
   }
 }
 
@@ -19,13 +31,6 @@ const parse = async () => {
 // These updates will come from the service worker.
 chrome.runtime.onMessage.addListener((msg, sender) => {
   if (msg.type === 'RUN_UPDATE') {
-    parse();
+    checkPage();
   }
-});
-
-
-kuromoji.builder({ dicPath: chrome.runtime.getURL('dict') }).build(function (err, tokenizer) {
-    // tokenizer is ready
-    var path = tokenizer.tokenize("すもももももももものうち");
-    console.log(path);
 });
