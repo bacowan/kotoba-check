f2b020b873ba89039198118eb64d26a6e2724ba0
bacowan <cowan.brendan.a@gmail.com>
2025-08-02 23:43:31 +0900

    no headers test

--- tests/unit/export.test.js
+++ tests/unit/export.test.js
@@ -1,5 +1,3 @@
-
-
 jest.mock('../../src/visualizer/utils');
 
 const enums = require('../../src/common/enums');
@@ -11,7 +9,7 @@ describe('exportDeck', () => {
     });
 
     afterEach(() => {
-        jest.restoreAllMocks();
+        jest.clearAllMocks();
     });
 
     it('includes headers when instructed to', () => {
@@ -36,8 +34,35 @@ describe('exportDeck', () => {
         );
 
         const parseCsvCalledParam = utils.parseCsv.mock.calls[0][0]; // 0,0 is the first param of the first call
+        expect(parseCsvCalledParam.length).toBe(2); // row for header and row for content
         expect(parseCsvCalledParam[0].length).toBe(1); // 0 is the first row
         expect(parseCsvCalledParam[0][0]).toBe('header'); // 0,0 is the first column of thee first row
+        expect(utils.download).toHaveBeenCalledWith(expect.any(String), "Correct");
+    });
+
+    it('excludes headers when instructed to', () => {
+        utils.parseCsv.mockReturnValue("Correct");
+        exportDeck.exportDeck(
+            [
+                {
+                    localStorageKey: "key",
+                    header: "header",
+                    export: word => word.word
+                }
+            ],
+            [
+                {
+                    word: "word1",
+                    count: 1,
+                    state: enums.CardState.InDeck,
+                    kuromojiId: 1
+                }
+            ],
+            false
+        );
+
+        const parseCsvCalledParam = utils.parseCsv.mock.calls[0][0]; // 0,0 is the first param of the first call
+        expect(parseCsvCalledParam.length).toBe(1); // just row for content
         expect(utils.download).toHaveBeenCalledWith(expect.any(String), "Correct");
     });
 });
