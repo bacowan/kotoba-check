8d6481ca03a90d91995e6c66b3ffd4bcf351f343
bacowan <cowan.brendan.a@gmail.com>
2025-08-10 18:18:30 +0900

    one more test

--- tests/unit/export.test.js
+++ tests/unit/export.test.js
@@ -65,4 +65,54 @@ describe('exportDeck', () => {
         expect(parseCsvCalledParam.length).toBe(1); // just row for content
         expect(utils.download).toHaveBeenCalledWith(expect.any(String), "Correct");
     });
+
+    it('includes all data', () => {
+        exportDeck.exportDeck(
+            [
+                {
+                    localStorageKey: "key1",
+                    header: "word",
+                    export: word => word.word
+                },
+                {
+                    localStorageKey: "key2",
+                    header: "count",
+                    export: word => word.count
+                },
+                {
+                    localStorageKey: "key2",
+                    header: "hi",
+                    export: word => "hi"
+                }
+            ],
+            [
+                {
+                    word: "word1",
+                    count: 1,
+                    state: enums.CardState.InDeck,
+                    kuromojiId: 1
+                },
+                {
+                    word: "word2",
+                    count: 2,
+                    state: enums.CardState.InDeck,
+                    kuromojiId: 2
+                }
+            ],
+            false
+        );
+
+        const parseCsvCalledParam = utils.parseCsv.mock.calls[0][0]; // 0,0 is the first param of the first call
+        expect(parseCsvCalledParam.length).toBe(2); // just rows for content
+
+        expect(parseCsvCalledParam[0].length).toBe(3); // 3 different columns
+        expect(parseCsvCalledParam[0][0]).toBe('word1');
+        expect(parseCsvCalledParam[0][1]).toBe(1);
+        expect(parseCsvCalledParam[0][2]).toBe('hi');
+
+        expect(parseCsvCalledParam[1].length).toBe(3); // 3 different columns
+        expect(parseCsvCalledParam[1][0]).toBe('word2');
+        expect(parseCsvCalledParam[1][1]).toBe(2);
+        expect(parseCsvCalledParam[1][2]).toBe('hi');
+    });
 });
