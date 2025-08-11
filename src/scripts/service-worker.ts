f15e93e3059c574f9bd2e86b90ee0f48b373fb23
bacowan <cowan.brendan.a@gmail.com>
2025-08-11 19:35:13 +0900

    forgot to wrap service working in onInstalled call

--- src/scripts/service-worker.ts
+++ src/scripts/service-worker.ts
@@ -1,10 +1,12 @@
-// Run an update whenever the URL changes via the History API
-chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
-  // Send message to the tab where the content script is running
-  chrome.tabs.sendMessage(details.tabId, { type: 'RUN_UPDATE' });
-});
+chrome.runtime.onInstalled.addListener(() => {
+  // Run an update whenever the URL changes via the History API
+  chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    // Send message to the tab where the content script is running
+    chrome.tabs.sendMessage(details.tabId, { type: 'RUN_UPDATE' });
+  });
 
-// Run an update whenever the URL changes via regular navigation
-chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.tabs.sendMessage(details.tabId, { type: 'RUN_UPDATE' });
+  // Run an update whenever the URL changes via regular navigation
+  chrome.webNavigation.onCompleted.addListener((details) => {
+    chrome.tabs.sendMessage(details.tabId, { type: 'RUN_UPDATE' });
+  });
 });
